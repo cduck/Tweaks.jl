858ca6ad220cf40e4de153d5a7a61860e09101b9
Casey Duckering <cduckering@gmail.com>
2020-08-24 13:45:18 -0500

    Support latex bar plot

--- src/Plot.jl
+++ src/Plot.jl
@@ -2,6 +2,7 @@ module Plot
 
 import PyPlot; plt = PyPlot
 plt.svg(true)
+using PyCall
 
 export plt, make_bar_plot
 
@@ -36,9 +37,11 @@ ax.set_ylabel("...")
 ```
 """
 function make_bar_plot(lookup_value::Function, groups, keys;
-                       colors=nothing, ax=nothing, width::Real=0.7,
-                       gap::Real=0.2, label_rotation::Real=0.0,
+                       colors=nothing, patterns=nothing, ax=nothing,
+                       width::Real=0.7, gap::Real=0.2, label_rotation::Real=0.0,
                        label_anchor=nothing)
+    is_latex = ax isa PyObject && startswith(ax.__module__, "latextools")
+
     n_groups = length(groups)
     n_bars = length(keys)
     width /= n_bars
@@ -53,18 +56,29 @@ function make_bar_plot(lookup_value::Function, groups, keys;
         fig = nothing
     end
 
+    group_strs = [g isa Pair ? g[1] : string(g) for g in groups]
+    groups = [g isa Pair ? g[2] : g for g in groups]
     for (i, key) in enumerate(keys)
-        key_str, key = (key isa Pair ? (string(key[1]), key[2])
+        key_str, key = (key isa Pair ? (key[1], key[2])
                         : (string(key), key))
         vals = [lookup_value(g isa Pair ? g[2] : g, key) for g in groups]
         c = colors !== nothing && i <= length(colors) ? colors[i] : nothing
-        ax.bar(x_arr .+ (offset + i*step), vals, width, color=c, label=key_str)
+        p = (patterns !== nothing && i <= length(patterns)
+             ? patterns[i] : nothing)
+        if is_latex
+            ax.bar(group_strs, vals, color=c, pattern=p, legend=key_str,
+                   extra_config="pattern color=$c,")
+        else
+            ax.bar(x_arr .+ (offset + i*step), vals, width, color=c,
+                   label=key_str)
+        end
     end
 
-    ax.set_xticks(collect(1:n_groups))
-    ax.set_xticklabels([string(g isa Pair ? g[1] : g) for g in groups],
-                       rotation=label_rotation, ha=label_anchor)
-    ax.legend()
+    if !is_latex
+        ax.set_xticks(collect(1:n_groups))
+        ax.set_xticklabels(group_strs, rotation=label_rotation, ha=label_anchor)
+        ax.legend()
+    end
     fig === nothing ? nothing : (fig, ax)
 end
 
